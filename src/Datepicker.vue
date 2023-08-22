8145bd5e885c87daa3ec7cfebb70682ba3ae3e14
Victor Reis Matos <97291820+Victorrmatos@users.noreply.github.com>
2023-08-22 12:27:38 +0100

    Create Datepicker.vue

---
+++ src/Datepicker.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="box">
+    <h1>Flatpickr + Vue 3</h1>
+    <flat-pickr v-model="date" :config="config" />
+    <hr />
+    v-model: {{ date }}
+  </div>
+</template>
+
+<script>
+import flatPickr from "vue-flatpickr-component";
+import "flatpickr/dist/flatpickr.css";
+
+export default {
+  data() {
+    return {
+      date: null,
+      config: {
+        inline: true,
+      },
+    };
+  },
+  components: {
+    flatPickr,
+  },
+};
+</script>
+
+<style>
+body {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+}
+</style>
